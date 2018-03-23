867d1d084e1acaa540128f1f60862a603602550a
Phoebe Lau <phoebelau7@gmail.com>
2018-03-23 18:49:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ const fareDoubler = createFareMultiplier(2)
 const fareTripler = createFareMultiplier(3)
 
 const selectDifferentDrivers = function(drivers, function){
-  return function(drivers);
+  function(drivers);
 }
