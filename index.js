8141e76d0f67b61719d7f10fa4c6ef4e9472ee86
Phoebe Lau <phoebelau7@gmail.com>
2018-03-23 18:43:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,4 @@ const returnLastTwoDrivers = function(drivers){
   return drivers.slice(-2)
 }
 
-const selectingDrivers = []
-const selectingDrivers.push(returnFirstTwoDrivers,returnLastTwoDrivers)
+const selectingDrivers = [].push(returnFirstTwoDrivers,returnLastTwoDrivers)
